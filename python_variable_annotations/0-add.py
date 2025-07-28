f8477f989449628ad0f7c79ef5b95adec97be4b7
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-28 12:15:05 +0300

    Initial commit

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env
+"""Module that contains a function to add two floats."""
+
+def add(a: float, b: float) -> float:
+    """Add two float numbers and return the result."""
+    return a + b
+
+
+
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+#!/usr/bin/env python3
