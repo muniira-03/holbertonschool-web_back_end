c5ed44442ac58405fea7a2808fdb0170c567ba41
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-31 11:50:56 +0300

    .

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 """Module that contains a function to add two floats."""
+
 
 def add(a: float, b: float) -> float:
     """Add two float numbers and return the result."""
     return a + b
-
-
-
