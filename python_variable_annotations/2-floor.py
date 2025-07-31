2557d9a2378f43189126d968499951d4af18e287
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-07-31 11:52:37 +0300

    .

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Module that contains a function to return the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an integer."""
+    return math.floor(n)
