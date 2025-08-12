c038d0b3fe3d1a3c9a673a870ff663b45d416bd0
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-08-12 12:54:00 +0300

    .

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,20 +1,43 @@
 #!/usr/bin/env python3
-"""Simple pagination example"""
-
+"""
+Implement a method named get_page that takes two integer arguments:
+page (default 1) and page_size (default 10).
+"""
 import csv
+import math  # noqa: F401
 from typing import List
-from simple_helper_function import index_range
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculate the start and end indices for a given page and page size.
+
+    Args:
+        page (int): The page number (1-based index).
+        page_size (int): The number of items per page.
+    Returns:
+        Tuple[int, int]: A tuple containing the start index
+         and end index for the given page.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """
+    Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """
+        Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -25,20 +48,17 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Get a specific page from the dataset.
-
-        Args:
-            page (int): Current page number (1-indexed).
-            page_size (int): Number of items per page.
-
-        Returns:
-            List[List]: A list of rows for the requested page.
+            Validate the input types and values.
+            Then load the data set and return it
         """
-
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        start, end = index_range(page, page_size)
+        range_tuple = index_range(page, page_size)
+        start = range_tuple[0]
+        end = range_tuple[1]
+
+        # Load the data set
         dataset = self.dataset()
 
         if start >= len(dataset):
