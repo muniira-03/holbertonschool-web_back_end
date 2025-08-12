63bd38012e2eb8535c8d3d6f73cf7340b56dec3c
muniira-03 <munirra.alsubaie@gmail.com>
2025-08-12 11:48:49 +0300

    .

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List, Dict, Any
+from simple_helper_function import index_range  # Make sure the filename is correct
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        data = self.get_page(page, page_size)
+        dataset_len = len(self.dataset())
+        total_pages = math.ceil(dataset_len / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
