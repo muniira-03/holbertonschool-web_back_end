d30cac8d5a576b64ceafc3d81cb5137f7eac881b
muniira-03 <munirra.alsubaie@gmail.com>
2025-08-17 20:58:34 +0300

    .

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
+"""8-all.py: lists all documents in a collection"""
 
+def list_all(mongo_collection):
+    """Return all documents in a PyMongo collection as a list"""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
