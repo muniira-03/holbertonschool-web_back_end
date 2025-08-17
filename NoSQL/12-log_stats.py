c5ee9e7ae2c5570717694a3c7864946d26e411c6
muniira-03 <munirra.alsubaie@gmail.com>
2025-08-17 21:04:44 +0300

    .

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
+"""12-log_stats.py: provide stats about Nginx logs stored in MongoDB"""
 
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods stats
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Status check: method=GET and path=/status
+    status_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
