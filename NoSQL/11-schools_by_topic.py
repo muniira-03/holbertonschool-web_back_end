1ed09ca176c41a8c71ab692ae48884342933bb72
muniira-03 <munirra.alsubaie@gmail.com>
2025-08-17 21:03:19 +0300

    .

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
+"""11-schools_by_topic.py: list schools having a specific topic"""
 
+def schools_by_topic(mongo_collection, topic):
+    """
+    Return the list of schools having a specific topic.
+
+    Args:
+        mongo_collection: PyMongo collection object
+        topic (str): topic to search for
+
+    Returns:
+        list: list of school documents containing the topic
+    """
+    if mongo_collection is None or topic is None:
+        return []
+
+    return list(mongo_collection.find({"topics": topic}))
