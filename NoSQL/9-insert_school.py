794de91887fec5b0aec95bef09aacfd99b701b51
muniira-03 <munirra.alsubaie@gmail.com>
2025-08-17 21:00:09 +0300

    .

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+"""9-insert_school.py: inserts a new document in a collection"""
 
+def insert_school(mongo_collection, **kwargs):
+    """Insert a document into a PyMongo collection using kwargs"""
+    if mongo_collection is None or not kwargs:
+        return None
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
