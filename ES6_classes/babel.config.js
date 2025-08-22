2bf4cc928b51ee4a6006a7ad4b977b256679bdd5
muniira-03 <munirra.alsubaie@gmail.com>
2025-08-22 03:25:27 +0300

    .

--- ES6_classes/babel.config.js
+++ ES6_classes/babel.config.js
@@ -1,4 +1,12 @@
-export default {
-    presets: ['@babel/preset-env'],
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
   };
-  
