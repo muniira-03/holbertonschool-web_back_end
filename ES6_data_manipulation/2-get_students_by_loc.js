b7a5f4c3431b431851c12ed5b830a2fb456a8046
Munira Alsubaie <munirra.alsubaie@gmail.com>
2025-08-30 03:13:44 +0300

    Update 2-get_students_by_loc.js

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,6 @@
-
+export default function getStudentsByLocation(listStudents, city) {
+  if (!Array.isArray(listStudents)) {
+    return [];
+  }
+  return listStudents.filter((student) => (student.location === city));
+}
